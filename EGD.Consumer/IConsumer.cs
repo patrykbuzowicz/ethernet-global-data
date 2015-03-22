6eb8b6b69f4ffd0d805e962acdc264ccff834e16
pluskwa <pluskwa.w@gmail.com>
2015-03-22 19:03:51 +0100

    Udp update

--- EGD.Consumer/IConsumer.cs
+++ EGD.Consumer/IConsumer.cs
@@ -15,45 +15,5 @@ namespace EGD.Consumer
         event EventHandler<DataReceivedEventArgs> DataReceived;
         void Close();
     }
-
-    public class Udpconsumer : IConsumer
-    {
-        public UdpClient udpClient;
-        public event EventHandler DatagramReceived;
-        event EventHandler handler;
-        public Byte[] receiveBytes;
-        bool done = false;
-        public void Open(string address)
-        {
-            done = false;
-            handler=null;
-            udpClient = new UdpClient(4746);
-            udpClient.Connect(address, 4746);
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-            while(!done)
-            {
-                receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
-                if(receiveBytes!=null)
-                {
-                handler = DatagramReceived;
-                handler(this, EventArgs.Empty);
-                handler = null;
-                receiveBytes = null;
-                }
-            }
-        }
-        public void Close() 
-        {
-            done = true;
-            udpClient.Close();
-        }
-}
-    public class ConsumerObserver
-    {
-        public void HandleEvent(object sender, EventArgs args)
-        {
-            Console.WriteLine("Datagram received " + ((Udpconsumer)sender).receiveBytes);
-            //przekazanie ramki do egd
-        }
-    }
+        
 }
---
+++ EGD.Consumer/Udp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EGD.Consumer
+{
+    class Udp
+    {
+        
+        event EventHandler<DatagramReceivedEventArgs> DatagramReceived;
+        DatagramReceivedEventArgs args;
+        public UdpClient udpClient;
+        event EventHandler<DatagramReceivedEventArgs> handler;
+        public Byte[] receiveBytes;
+        bool done;
+        public IPEndPoint RemoteIpEndPoint;
+        public Udp()
+        {
+            done = false;
+        }
+        public void Open(string address)
+        {
+            done = false;
+            handler=null;
+            udpClient = new UdpClient(4746);
+            udpClient.Connect(address, 4746);
+            RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            while(!done)
+            {
+                receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                if(receiveBytes!=null)
+                {
+                args=new DatagramReceivedEventArgs(receiveBytes);
+                handler = DatagramReceived;
+                handler(this, args);
+                handler = null;
+                receiveBytes = null;
+                }
+            }
+        }
+        public void Close() 
+        {
+            done = true;
+            udpClient.Close();
+        }
+}
+    public class UdpObserver
+    {
+        public void HandleEvent(object sender, DatagramReceivedEventArgs args)
+        {
+            string str=System.Text.Encoding.UTF8.GetString(args.Bytes);
+            Console.WriteLine("Datagram received " + str);
+            //przekazanie ramki do egd
+        }
+    }
+}
