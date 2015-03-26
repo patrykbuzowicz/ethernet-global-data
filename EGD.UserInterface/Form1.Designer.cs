9e8387652e6d9131e8fcdf4bfc2e5b6dd8de44d8
Unpredictable007 <artur.grabowski92@gmail.com>
2015-03-26 10:06:50 +0100

    Końcowe zmiany w GUI

--- EGD.UserInterface/Form1.Designer.cs
+++ EGD.UserInterface/Form1.Designer.cs
@@ -50,6 +50,12 @@
             this.OpenConnetionButton = new System.Windows.Forms.Button();
             this.CloseConnectionButton = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -71,7 +77,7 @@
             this.panel1.Controls.Add(this.checkBox9);
             this.panel1.Controls.Add(this.checkBox2);
             this.panel1.Controls.Add(this.checkBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(332, 65);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(88, 184);
             this.panel1.TabIndex = 0;
@@ -256,7 +262,7 @@
             // 
             this.textBox1.Enabled = false;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(106, 12);
+            this.textBox1.Location = new System.Drawing.Point(473, 65);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 40);
             this.textBox1.TabIndex = 1;
@@ -264,7 +270,7 @@
             // lbLastUpdated
             // 
             this.lbLastUpdated.AutoSize = true;
-            this.lbLastUpdated.Location = new System.Drawing.Point(12, 218);
+            this.lbLastUpdated.Location = new System.Drawing.Point(23, 230);
             this.lbLastUpdated.Name = "lbLastUpdated";
             this.lbLastUpdated.Size = new System.Drawing.Size(71, 13);
             this.lbLastUpdated.TabIndex = 2;
@@ -272,7 +278,7 @@
             // 
             // OpenConnetionButton
             // 
-            this.OpenConnetionButton.Location = new System.Drawing.Point(221, 9);
+            this.OpenConnetionButton.Location = new System.Drawing.Point(193, 82);
             this.OpenConnetionButton.Name = "OpenConnetionButton";
             this.OpenConnetionButton.Size = new System.Drawing.Size(75, 23);
             this.OpenConnetionButton.TabIndex = 4;
@@ -282,7 +288,7 @@
             // 
             // CloseConnectionButton
             // 
-            this.CloseConnectionButton.Location = new System.Drawing.Point(311, 9);
+            this.CloseConnectionButton.Location = new System.Drawing.Point(193, 128);
             this.CloseConnectionButton.Name = "CloseConnectionButton";
             this.CloseConnectionButton.Size = new System.Drawing.Size(75, 23);
             this.CloseConnectionButton.TabIndex = 5;
@@ -293,17 +299,80 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(106, 61);
+            this.listBox1.Location = new System.Drawing.Point(26, 65);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(290, 82);
+            this.listBox1.Size = new System.Drawing.Size(99, 121);
             this.listBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Connecion options:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "- Choose IP Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "- Open/ Close connection";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(319, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Data from PLC controler";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(329, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "- Flags";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(470, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "- Number";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(575, 315);
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(592, 270);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.CloseConnectionButton);
             this.Controls.Add(this.OpenConnetionButton);
@@ -344,6 +413,12 @@
         private System.Windows.Forms.Button OpenConnetionButton;
         private System.Windows.Forms.Button CloseConnectionButton;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
